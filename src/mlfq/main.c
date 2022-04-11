5d51f2cb39edc89e8ec62558de3dba9ace5f390c
Luis González <ljgonzalez@uc.cl>
2022-04-10 22:40:21 -0400

    Formato

--- src/mlfq/main.c
+++ src/mlfq/main.c
@@ -29,8 +29,8 @@
 # include "errors/errors.h"
 
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
+
 	/*Lectura del input*/
 	char *file_name = (char *)argv[1];
 	InputFile *input_file = read_file(file_name);
@@ -39,12 +39,12 @@ int main(int argc, char const *argv[])
 	printf("Nombre archivo: %s\n", file_name);
 	printf("Cantidad de procesos: %d\n", input_file->len);
 	printf("Procesos:\n");
-	for (int i = 0; i < input_file->len; ++i)
-	{
-		for (int j = 0; j < 7; ++j)
-		{
+
+	for (int i = 0; i < input_file->len; ++i)	{
+		for (int j = 0; j < 7; ++j)	{
 			printf("%s ", input_file->lines[i][j]);
 		}
+
 		printf("\n");
 	}
 
