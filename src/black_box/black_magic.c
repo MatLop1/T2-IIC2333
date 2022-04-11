c25a4bb8cc87a5a9f340a6186890993957c22c7b
Luis González <ljgonzalez@uc.cl>
2022-04-10 23:44:02 -0400

    Avances

--- src/black_box/black_magic.c
+++ src/black_box/black_magic.c
@@ -31,20 +31,38 @@ int get_quantum(int q_value, int priority) {  //
   return quantum_val;
 }
 
+void run_fifo(int q, int priority, Queue queue1, Queue queue2, Queue queue3) {
+  int quantum = get_quantum(q, priority)
+  //TODO: guardar quantum restante en "algún" lugar
+
+  // TODO: Asignar el tipo de variable al proceso
+  if (priority == 2) {
+    Process *process = queue1.pop(0);
+
+  } else {
+    Process *process = queue2.pop(0);
+  }
+
+  process.do_stuff()
+
+
+}
+
 // Hace pasar un ciclo.
 // Retorna falso si no hay más programas en la cola
-//FIXME: Potencial bug: Si pasa
-bool tick(Queue queue1, Queue queue2, Queue queue3) {
+//FIXME: Si pasa que el CPU no tiene nada que hacer, pero dsps van
+// a llegar programas, entonces va a marcar como que terminó.
+bool tick(int q, Queue queue1, Queue queue2, Queue queue3) {
   load_programs()
 
   if (queue1.size > 0) {
-    run_fifo(queue1);
+    run_fifo(int q, 2, queue1, queue2, queue3);
 
   } else if (queue2.size > 0) {
-    run_fifo(queue2);
+    run_fifo(int q, 1, queue1, queue2, queue3);
 
   } else if (queue3.size > 0) {
-    run_sjf(queue3);
+    run_sjf(queue1, queue2, queue3);
 
   } else {
     return false;
@@ -53,19 +71,3 @@ bool tick(Queue queue1, Queue queue2, Queue queue3) {
 
   return true;
 }
-
-while procesos.existen()
-checkeo_listas()
-0x#### <-- lista#.pop()
-
-checkear_nueva_lista(0x####)
-lista[#+1].push() --> 0x####
-
-calculo_tiempo_exec
-
-for tick in t_exec_l#=10:
-lista1.procesar_tick()
-lista2.procesar_tick()
-lista3.procesar_tick()
-
-lista3.pop() <inlcuye sort>
