eecc67e017c6ff41348ec499348e0c5b95228bf3
Luis González <ljgonzalez@uc.cl>
2022-04-10 22:48:07 -0400

    Incluyo al archivo

--- compilar-y-ejecutar.sh
+++ compilar-y-ejecutar.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+clear
+
 FILE_NAME="mlfq"
 
 echo -e "\033[33;1mMake clean\033[0m"
@@ -12,5 +14,5 @@ echo -e "\033[33;1mHaciendo ejecutable\033[0m"
 chmod +x "./$FILE_NAME"
 
 echo -e "\033[33;1mCorriendo con valgrind\033[0m"
-valgrind --leak-check=full -s "./$FILE_NAME"
+valgrind --leak-check=full -s "./$FILE_NAME" "./example.txt"
 echo -e "\033[33;1mFin\033[0m"
