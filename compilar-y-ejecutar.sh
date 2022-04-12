5f79be1845b099828b8c2f4a3673093bdb956ce3
Luis González <ljgonzalez@uc.cl>
2022-04-12 05:15:08 -0400

    Uso de inputs

--- compilar-y-ejecutar.sh
+++ compilar-y-ejecutar.sh
@@ -4,6 +4,22 @@ clear
 
 FILE_NAME="mlfq"
 
+if [[ $1 == "" ]]; then
+    ARG1="out.txt"
+
+else
+    ARG1=$1
+
+fi
+
+if [[ $2 == "" ]]; then
+    ARG2="5"
+
+else
+    ARG2=$2
+
+fi
+
 echo -e "\033[33;1mMake clean\033[0m"
 make clean
 
@@ -14,5 +30,5 @@ echo -e "\033[33;1mHaciendo ejecutable\033[0m"
 chmod +x "./$FILE_NAME"
 
 echo -e "\033[33;1mCorriendo con valgrind\033[0m"
-valgrind --leak-check=full -s "./$FILE_NAME" "./example.txt"
+valgrind --leak-check=full -s "./$FILE_NAME" "./example.txt" "$ARG2" "$ARG3"
 echo -e "\033[33;1mFin\033[0m"
